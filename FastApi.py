648c182910cf21322d2caf9385a245d62bd63fb4
Isa Escribano <isaesgo12@gmail.com>
2025-09-04 20:47:37 +0200

    Update FastApi.py

--- FastApi.py
+++ FastApi.py
@@ -34,7 +34,7 @@ class HealthResponse(BaseModel):
     message: str
 
 # Global variables for model and encoders
-model = 'https://github.com/IEG-hub/FastAPI_ML/tree/main/models'
+model = 'models/xgb_final_model.pkl'
 encoders = None
 
 # Load model on startup
@@ -43,7 +43,7 @@ async def load_model():
     global model, encoders
     try:
         # Update this path to your actual model path
-        path_model = 'https://github.com/IEG-hub/FastAPI_ML/tree/main/models'
+        path_model = 'models/xgb_final_model.pkl'
         file_name = 'xgb_final_model.pkl'
         model_path = os.path.join(path_model, file_name)
         
